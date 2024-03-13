e70a0481a61fe2fef966dacde9ec4a29538afa0f
Harison Rios de Miranda <hharison562@gmail.com>
2024-03-13 01:11:59 -0300

    testedeploy

--- src/components/commons/projectcard.tsx
+++ src/components/commons/projectcard.tsx
@@ -5,7 +5,7 @@ function ProjectCard() {
   return (
     <Link to="../../Projects.tsx">
       <div className="item project-card"> 
-        <img src="https://tenor.com/pt-BR/view/minecraft-crystal-gif-24596905" alt="enchatedbook.gif"/>
+        <img src="../../public/home/enderchest.gif" alt="enchatedbook.gif"/>
         <h2>Projetos</h2>
       </div>
     </Link>
